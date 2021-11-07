8d153efd4a234d44077498b6424c6f8f94a4f06c
jogerj <30559735+jogerj@users.noreply.github.com>
2021-11-07 17:04:22 +0100

    Added start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+MC="paper.jar"
+MC_UPDATE="paper_update.jar"
+
+# Replace if update exists
+if test -f "$MC_UPDATE"; then
+    echo "Updating $MC..."
+    mv $MC $MC.old
+    mv $MC_UPDATE $MC
+else
+    echo "No JAR updates found."
+fi
+
+# SAFE MODE
+# screen -dmS minecraft java -Xms2G -Xmx2G -jar $MC --nogui
+
+# Default optimized (Aikar's flags)
+# screen -dmS minecraft java -Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MC --nogui
+
+# Optimized (personalized for my rig with 24GB RAM)
+screen -dmS minecraft java -Xms16G -Xmx16G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar $MC --nogui
